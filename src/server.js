fc36751626dc333576c902da125a319c9770bebb
AndreaD148 <dardy148@gmail.com>
2020-09-14 10:44:50 +0200

    Initial commit
    Initialize connection between server and postgres DB

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const { Client } = require("pg");
+
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(helmet());
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+
+  client.connect();
+  const query = "SELECT * FROM react_user";
+
+  client.query(query, (res, err) => {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    console.log(res);
+  })
+
+})
+
