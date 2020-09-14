2299436ae2a75be444fd71272fc3ee696534a911
AndreaD148 <dardy148@gmail.com>
2020-09-14 17:08:32 +0200

    Adding body-parser

--- src/server.js
+++ src/server.js
@@ -1,41 +1,26 @@
 const express = require("express");
 const helmet = require("helmet");
 
-const { Client } = require("pg");
-
 require("dotenv").config();
 
 const app = express();
+const router = require("./router");
 
 const PORT = process.env.PORT || 4000;
 
 app.use(helmet());
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
 
-const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
-})
 
+app.use('/', router);
 
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
-
-  client.connect();
-  const query = `SELECT * FROM react_user`;
-
-  client.query(query, (err, res) => {
-    if(err) {
-      throw err;
-    }
-
-    console.table(res.rows)
-
-  }) 
-
-
 })
 
